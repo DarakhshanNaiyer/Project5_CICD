5c19130f80939973de489e8a46955fab486556f0
Darakhshan Naiyer <darakhsha18795@gmail.com>
2021-09-19 19:31:14 +0530

    Project5_CICD 5th commit

--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-isExistApp = `pgrep httpd`
-if [[ -n  $isExistApp ]]; then
-    service httpd stop        
-fi
+#isExistApp = `pgrep httpd`
+#if [[ -n  $isExistApp ]]; then
+#   service httpd stop        
+#fi
+
+echo "abc"
